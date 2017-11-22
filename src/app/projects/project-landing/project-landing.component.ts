00eaed0d6c5b3a8f03ddf696e3bb61eda3900b5f
Burch, Ryan <rburch@silverstarbrands.com>
2017-11-22 11:19:27 -0600

    2nd try

--- src/app/projects/project-landing/project-landing.component.ts
+++ src/app/projects/project-landing/project-landing.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import * as RecordRTC from 'recordrtc';
+// import '../../../assets/js/recorderFunctions.js';
+// import '../../../assets/js/recorder.js';
+
+
+declare function startRecording(button): void;
+declare function stopRecording(button): void;
+declare var recorderObject: any;
 
 @Component({
   selector: 'app-project-landing',
@@ -9,57 +15,20 @@ import * as RecordRTC from 'recordrtc';
 export class ProjectLandingComponent implements OnInit {
 
 
-  private stream: MediaStream;
-  private recordRTC: any;
-  private recordedFile: any;
-
-  @ViewChild('audio') audio;
-
   constructor() { }
   
-  startRecording() {
-
-    // set the media constraints for audio only.
-    let mediaConstraints = {
-      video: false,
-      audio: true
-    };
-
-    // Navigator is an object that refers to the browser, we can access media devices like the Mic.
-    // Will return a stream which we can use. 
-    
-    navigator.mediaDevices.getUserMedia(mediaConstraints).then((stream) => {
-      this.successConnecting(stream);
-    }).catch((err) => {
-      console.log(err);
-      console.log('Cannot access Microphone')
-    })
-
-}
-
-  successConnecting(stream: MediaStream) {
-    // Use the recordRTC library. 
-    this.recordRTC = RecordRTC(stream, { type: 'audio', mimeType: 'mp3'});
-    this.recordRTC.startRecording();
+  start(button){
+  	startRecording(button);
+  };
+  
+  
+  stop(button){
+  	stopRecording(button);
   }
-
-  stopRecording() {
-    this.recordRTC.stopRecording((audioURL) => {
-      this.audio.nativeElement.src = audioURL; // Set the audio element src for playback. 
-      this.recordedFile = this.recordRTC.getBlob(); // Save audio file to variable 
-      // save audio file to S3 bucket 
-   //   console.log(recordedFile)
-    });
-  }
-
-  saveRecording() {
-    this.recordRTC.save('audio.webm');
-  }
-
  
 
   ngOnInit() {
-  
+    recorderObject.recorder();
 
   }
 
