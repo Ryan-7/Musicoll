44b07f03c5d2f9b8d23165ae90c724667c29946c
Burch, Ryan <rburch@silverstarbrands.com>
2017-12-01 12:04:17 -0600

    recorder working

--- src/app/recorder/recorder.component.ts
+++ src/app/recorder/recorder.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
-
-
 import * as RecordRTC from 'recordrtc';
 
 declare const navigator: any;
@@ -38,7 +36,6 @@ export class RecorderComponent implements OnInit, OnDestroy {
     this.recording = false;
     this.mediaRecorder.stop();
 
-
     this.mediaRecorder.onstop = (e) => {
       const audio = new Audio();
       const blob = new Blob(this.chunks, { 'type': 'audio/ogg; codecs=opus' });
@@ -46,22 +43,10 @@ export class RecorderComponent implements OnInit, OnDestroy {
       var url = (window.URL).createObjectURL(blob)
       this.downloadAudio.nativeElement.download = "output.ogg";
       this.downloadAudio.nativeElement.href = url;
-      this.audioPlayback.nativeElement.src = url
+      this.audioPlayback.nativeElement.src = url;
     };
 
-    // this.audioRecorder.exportWAV((blob) => {
-    //   var url = (window.URL).createObjectURL(blob);
-    //   this.audioPlayback.nativeElement.src = url;
-    //   console.log(this.audioPlayback.nativeElement.src)
-    //   if (this.audioPlayback.nativeElement.src !== "") {
-    //     this.recorderHasTrack = true;
-    //   }
-
-    //   this.downloadAudio.nativeElement.download = "output.wav";
-    //   this.downloadAudio.nativeElement.href = url;
-    //   console.log(this.downloadAudio)
-    // });
-
+    this.recorderHasTrack = true;
 
   }
 
@@ -82,52 +67,25 @@ export class RecorderComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
 
-    const onSuccess = (stream) => {
-      this.mediaRecorder = new MediaRecorder(stream);
-      this.mediaRecorder.ondataavailable = e => this.chunks.push(e.data);
-    };
+    let mediaConstraints = {
+      audio: {
+        echoCancellation: false,
+        echoCancelation: false, // TypeScript / IE Edge has a spelling error. 
+        noiseSuppression: false // FireFox, sounds terrible with this set to true while recording instruments.
+      } as any
+    }
 
+    navigator.mediaDevices.getUserMedia(mediaConstraints).then((stream) => {
+      this.streamSuccess(stream);
+    }).catch((e) => {
+      console.log(e);
+      this.errorRecording = true;
+    })
 
-
-    navigator.getUserMedia = (navigator.getUserMedia ||
-      navigator.webkitGetUserMedia ||
-      navigator.mozGetUserMedia ||
-      navigator.msGetUserMedia);
-
-    navigator.getUserMedia({ audio: {
-      echoCancellation: false
-    } }, onSuccess, e => console.log(e));
-
-
-
-    
-
-
-    // console.log(this.downloadAudio.nativeElement.href)
-    // this.audioPlayback.nativeElement.src = "";
-    // this.audioContext = new AudioContext();
-    
-    // let mediaConstraints = {
-    //   audio: {
-    //     echoCancellation: false,
-    //     echoCancelation: false, // TypeScript / IE Edge has a spelling error. 
-    //     noiseSuppression: false // FireFox, sounds terrible with this set to true while recording instruments.
-    //   } as any
-    // }
-    // console.log(navigator.mediaDevices.getSupportedConstraints()); // Differs by browser 
-
-    // navigator.mediaDevices.getUserMedia(mediaConstraints).then((stream) => {
-    //   this.streamSuccess(stream);
-    // }).catch((e) => {
-    //   console.log(e);
-    //   this.errorRecording = true;
-    // })
   }
 
   ngOnDestroy() {
-    // console.log('destroyed')
-    // this.audioContext.close();  // Prevent several audio instances
-    // this.recording = false; 
+     this.recording = false; 
   }
  
 }
